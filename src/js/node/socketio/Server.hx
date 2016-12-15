93e11af5d01577db60b9ca38fe6ac4919ef5358e
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-12-15 13:24:56 +0100

    Updated server

--- src/js/node/socketio/Server.hx
+++ src/js/node/socketio/Server.hx
@@ -8,16 +8,14 @@ extern class Namespace extends Socket
 @:native("require('socket.io')")
 extern class Server extends Namespace
 {
+    public var sockets:Namespace;
     public function new(?a:Dynamic, ?b:Dynamic);
-    public function serverClient(v:Bool):Server;
+    public function serveClient(v:Bool):Server;
     public function path(v:String):Server;
     public function adapter(v:Dynamic):Server;
     public function origins(v:String):Server;
     public function attach(srv_or_port:Dynamic, ?opts:Dynamic):Server;
     public function listen(srv_or_port:Dynamic, ?opts:Dynamic):Server;
     public function of(namespace:String):Namespace;
-
     public function close():Void;
-
-
 }
