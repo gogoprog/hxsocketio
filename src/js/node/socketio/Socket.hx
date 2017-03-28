fab7e9d6818036bae05a2bd20b1dc753bee92e94
Ignatiev Mikhail <ignatiev.reg@gmail.com>
2017-03-28 18:18:14 +0300

    externs update

--- src/js/node/socketio/Socket.hx
+++ src/js/node/socketio/Socket.hx
@@ -5,6 +5,8 @@ extern class Socket
     public var rooms:Array<String>;
     public var id:String;
 
+	@:overload(function (name:String, ?data:Dynamic, ?callback:Dynamic->Void):Void{})
+	@:overload(function (name:String, ?callback:Dynamic->Void):Void{})
     public function emit(name:String, ?data:Dynamic):Void;
     public function join(name:String, ?callback:Dynamic):Socket;
     public function leave(name:String, ?callback:Dynamic):Socket;
@@ -13,4 +15,9 @@ extern class Socket
     @:overload(function (event:String, ?func:Void->Void):Void{})
     @:overload(function (event:String, ?func:Dynamic->Dynamic->Void):Void{})
     public function on(event:String, ?func:Dynamic->Void):Void;
+	
+	
+    @:overload(function (event:String, ?func:Void->Void):Void{})
+    @:overload(function (event:String, ?func:Dynamic->Dynamic->Void):Void{})
+    public function once(event:String, ?func:Dynamic->Void):Void;
 }
